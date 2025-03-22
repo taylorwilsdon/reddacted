db172d6432e441f758914f693d8b6750a505b6fd
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-03-22 16:39:13 -0400

    release version

--- reddacted/_version.py
+++ reddacted/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.4.post1.dev1'
-__version_tuple__ = version_tuple = (0, 1, 4, 'dev1')
+__version__ = version = '0.1.2.post1.dev25'
+__version_tuple__ = version_tuple = (0, 1, 2, 'dev25')
