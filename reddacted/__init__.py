cb88dcc10358e8afa6b68e9d2bada12d97af9efa
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-04-04 15:57:33 -0400

    release v0.2.3

--- reddacted/__init__.py
+++ reddacted/__init__.py
@@ -1,3 +1,3 @@
 """reddacted - AI-Powered Reddit Privacy Suite"""
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
--- reddacted/version.py
+++ reddacted/version.py
@@ -1,4 +1,4 @@
 """Version information."""
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 version = __version__
